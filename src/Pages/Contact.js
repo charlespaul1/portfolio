f716a8d0965bdbd44b5e01d9c49715551212d04e
charlespaul1 <charlespaulwabomba@gmail.com>
2023-05-03 22:36:45 +0300

    styled the contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Box, Heading, Input, Textarea, Button, useToast} from '@chakra-ui/react'
-import {useFormik} from 'formik'
+import { Box, Heading, Flex, Input, Textarea, Button, useToast } from '@chakra-ui/react'
+import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
 const Contact = () => {
@@ -13,8 +13,8 @@ const Contact = () => {
   }
 
   const validationSchema = Yup.object({
-    name: Yup.string().required('Required'),
-    email: Yup.string().email('Invalid email format').required('Required'),
+    name: Yup.string().required('Name is Required'),
+    email: Yup.string().email('Invalid email format').required('Email is Required'),
     message: Yup.string().required('Message is required'),
   })
   const onSubmit = async (values, { setSubmitting, resetForm }) => {
@@ -52,63 +52,71 @@ const Contact = () => {
         Contact Me
       </Heading>
       <Box as='form' onSubmit={formik.handleSubmit}>
+        <Flex justifyContent='space-between' mb={4} gap={2}>
+
         <Input
-        id='name'
-        name='name'
-        type='text'
-        placeholder='Enter Your name'
-        value={formik.values.name}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        isInvalid={formik.touched.name && formik.errors.name}
-        mb={4}
+          id='name'
+          name='name'
+          type='text'
+          placeholder='Enter Your name'
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          isInvalid={formik.touched.name && formik.errors.name}
+          mb={4}
         />
+        
+        <Input
+          id='email'
+          name='email'
+          type='email'
+          placeholder='Enter Your email'
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          isInvalid={formik.touched.email && formik.errors.email}
+          mb={4}
+        />
+        </Flex>
         {formik.touched.name && formik.errors.name ? (
           <Box color='red' fontSize='sm'>
             {formik.errors.name}
-            </Box>
+          </Box>
         ) : null}
-        <Input
-        id='email'
-        name='email'
-        type='email'
-        placeholder='Enter Your email'
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        isInvalid={formik.touched.email && formik.errors.email}
-        mb={4}
-        />
+
         {formik.touched.email && formik.errors.email ? (
           <Box color='red' mb={4} fontSize='sm'>
             {formik.errors.email}
-            </Box>
+          </Box>
         ) : null}
         <Textarea
-        id='message'
-        name='message'
-        placeholder='Enter Your message'
-        value={formik.values.message}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        isInvalid={formik.touched.message && formik.errors.message}
-        mb={4}
+          id='message'
+          name='message'
+          placeholder='Enter Your message'
+          value={formik.values.message}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          isInvalid={formik.touched.message && formik.errors.message}
+          mb={4}
+          
+          h='200px'
+          variant={'filled'}
         />
         {formik.touched.message && formik.errors.message ? (
           <Box color='red' mb={4} fontSize='sm'>
             {formik.errors.message}
-            </Box>
+          </Box>
         ) : null}
         <Button
-        type='submit'
-        colorScheme='teal'
-        variant={formik.isSubmitting ? 'outline' : 'solid'}
-        isLoading={formik.isSubmitting}
+          type='submit'
+          colorScheme='teal'
+          variant={formik.isSubmitting ? 'outline' : 'solid'}
+          isLoading={formik.isSubmitting}
         >
           Send Message
         </Button>
       </Box>
-      </Box>
+    </Box>
   )
 }
 
